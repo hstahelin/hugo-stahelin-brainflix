a19c63c4371f2157a23b1be8eda298c6463642f2
HugoStahelin MacBookAir <hugo.stahelin@gmail.com>
2024-05-14 10:07:37 -0700

    Add Video Info component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Header from "./components/Header/Header";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 import videos from "./data/videos.json";
 import videoDetails from "./data/video-details.json";
+import VideoDescription from "./components/VideoDescription/VideoDescription";
 
 function App() {
   const [allVideos, setAllVideos] = useState(videos);
-  const [allVideoDetails, setAllVideoDetails] = useState(videoDetails);
-  const [mainVideo, setMainVideo] = useState(allVideoDetails[0]);
+  const [allVideosDetails, setAllVideoDetails] = useState(videoDetails);
+  const [mainVideo, setMainVideo] = useState(allVideosDetails[0]);
   return (
     <>
       <Header />
       <VideoPlayer video={mainVideo} />
+      <VideoDescription video={mainVideo} />
     </>
   );
 }
---
+++ src/components/VideoDescription/VideoDescription.jsx
@@ -0,0 +1,38 @@
+import "./VideoDescription.scss";
+
+import ViewsIcon from "../../assets/icons/views.svg";
+import LikesIcon from "../../assets/icons/likes.svg";
+
+function VideoDescription({ video }) {
+  const date = new Date(video.timestamp);
+  return (
+    <section className="video-description">
+      <h1 className="video-description__title page-header">{video.title}</h1>
+      <hr className="video-description__break" />
+      <div className="video-info">
+        <div className="video-info__info">
+          <h2 className="video-info__channel subheader">By {video.channel}</h2>
+          <p className="video-info__timestamp body-copy">
+            {date.toLocaleDateString("en-US")}
+          </p>
+        </div>
+        <div className="video-info__counters">
+          <div className="video-info__views">
+            <img src={ViewsIcon} alt="" className="video-info__views-icon" />
+            <p className="video-info__views-counter body-copy">{video.views}</p>
+          </div>
+          <div className="video-info__likes">
+            <img src={LikesIcon} alt="" className="video-info__likes-icon" />
+            <p className="video-info__likes-counter body-copy">{video.likes}</p>
+          </div>
+        </div>
+      </div>
+      <hr className="video-description__break" />
+      <p className="video-description__description body-copy">
+        {video.description}
+      </p>
+    </section>
+  );
+}
+
+export default VideoDescription;
