53387ba48c0f9f78f40d944faaf0f9766f577e8c
HugoStahelin MacBookAir <hugo.stahelin@gmail.com>
2024-06-18 21:19:57 -0700

    Fix Next videos card width

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -4,9 +4,7 @@ import VideoCard from "../VideoCard/VideoCard";
 function NextVideos({ nextVideos }) {
   return (
     <section className="next-videos">
-      <h3 className="next-videos__title subheader">
-        NEXT VIDEOS {nextVideos.length}
-      </h3>
+      <h3 className="next-videos__title subheader">NEXT VIDEOS</h3>
       <ul className="next-list">
         {nextVideos.map((video) => {
           return <VideoCard key={video.id} video={video} />;
