37b5889d7db058128d4e1dfc2dd3824879b0b394
HugoStahelin MacBookAir <hugo.stahelin@gmail.com>
2024-07-04 15:32:40 -0700

    Style thumbnail upload

--- src/components/VideoUpload/VideoUpload.jsx
+++ src/components/VideoUpload/VideoUpload.jsx
@@ -1,6 +1,6 @@
 import "./VideoUpload.scss";
 import Image from "../../assets/images/Upload-video-preview.jpg";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -16,6 +16,9 @@ function VideoUpload() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const [isImageValid, setIsImageValid] = useState(false);
+
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const fileInputRef = useRef(null);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -34,18 +37,16 @@ function VideoUpload() {
       newVideo.append("image", `${URL}/images/sample.jpg`);
       newVideo.append("file", formData.file);
 
-      console.log("newVideo: ", newVideo);
       const createdVideo = await postVideoData(newVideo);
       navigate(`/video/${createdVideo.id}`);
     } else {
       setError(
-        "Please fill out Title and Description and select a valid image file."
+        "Submission Error: Title, description, and image are all required to proceed."
       );
     }
   }
 
   async function postVideoData(video) {
-    console.log("inside POSTVIDEODATA: ", video);
     try {
       const response = await axios.post(`${URL}/videos`, video);
       return response.data;
@@ -56,6 +57,12 @@ function VideoUpload() {
 
   function handleCancel() {
     setFormData({ title: "", description: "", file: {} });
+    setPreviewUrl(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+    setIsImageValid(false);
+    setError("");
   }
 
   function handleFileChange(event) {
@@ -64,6 +71,12 @@ function VideoUpload() {
     if (selectedFile) {
       const fileType = selectedFile.type;
       if (validImageTypes.includes(fileType)) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setPreviewUrl(reader.result);
+        };
+        reader.readAsDataURL(selectedFile);
+
         setFormData({
           ...formData,
           file: event.target.files[0],
@@ -82,56 +95,65 @@ function VideoUpload() {
       <section className="video-upload">
         <h1 className="video-upload__title page-header">Upload Video</h1>
         <hr className="video-upload__divider" />
-        <div className="video-upload__content">
-          <form className="upload-form">
-            <label
-              htmlFor="thumbnail"
-              className="thumbnail__title labels-and-buttons"
-            >
-              VIDEO THUMBNAIL
-            </label>
-
-            <input
-              type="file"
-              name="thumbnail"
-              id="thumbnail"
-              className="thumbnail__upload"
-              onChange={handleFileChange}
-            />
-            {error && <p className="thumbnail__error">{error}</p>}
-
-            <label
-              htmlFor="title"
-              className="upload-form__label labels-and-buttons"
-            >
-              TITLE YOUR VIDEO
-            </label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="upload-form__input"
-              placeholder="Add a title to your video"
-              value={formData.title}
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="description"
-              className="upload-form__label labels-and-buttons"
-            >
-              ADD A VIDEO DESCRIPTION
-            </label>
-            <textarea
-              className="upload-form__input"
-              name="description"
-              id="description"
-              rows="4"
-              placeholder="Add a description to your video"
-              value={formData.description}
-              onChange={handleChange}
-            />
-          </form>
-        </div>
+        <form className="upload-form">
+          <div className="video-upload__content">
+            <div className="video-upload__thumbnail">
+              <label
+                htmlFor="thumbnail"
+                className="thumbnail__title labels-and-buttons"
+              >
+                VIDEO THUMBNAIL
+              </label>
+              <img
+                src={previewUrl || Image}
+                alt="Video upload thumbnail"
+                className="thumbnail__image"
+              />
+              <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                name="thumbnail"
+                id="thumbnail"
+                className="thumbnail__upload"
+                onChange={handleFileChange}
+              />
+            </div>
+            <div className="video-upload__info">
+              <label
+                htmlFor="title"
+                className="upload-form__label labels-and-buttons"
+              >
+                TITLE YOUR VIDEO
+              </label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                className="upload-form__input"
+                placeholder="Add a title to your video"
+                value={formData.title}
+                onChange={handleChange}
+              />
+              <label
+                htmlFor="description"
+                className="upload-form__label labels-and-buttons"
+              >
+                ADD A VIDEO DESCRIPTION
+              </label>
+              <textarea
+                className="upload-form__input"
+                name="description"
+                id="description"
+                rows="4"
+                placeholder="Add a description to your video"
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </form>
+        {error && <p className="thumbnail__error body-copy">{error}</p>}
         <hr className="video-upload__divider" />
         <div className="upload-form__buttons">
           <button
